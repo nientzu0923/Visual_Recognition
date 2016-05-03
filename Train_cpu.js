6d4cf1f762069b85a33f3bea0a5ba6bbeea69f58
nientzu0923 <nientzu0923.cs03g@nctu.edu.tw>
2016-05-03 12:29:57 +0800

    Update Train_cpu.js

--- Train_cpu.js
+++ Train_cpu.js
@@ -6,16 +6,16 @@ var fs = require('fs');
 
 
 var visual_recognition = watson.visual_recognition({
-  username: 'your username',
-  password: 'your password',
+  username: 'your username',  //這邊改你所拿到的username
+  password: 'your password',  //這邊改你所拿到的password
   version: 'v2-beta',
   version_date: '2015-12-02'
 });
 
 var params = {
   name: 'cpu',
-  positive_examples: fs.createReadStream('./cpupositive.zip'),
-  negative_examples: fs.createReadStream('./cpunegative.zip')
+  positive_examples: fs.createReadStream('./cpupositive.zip'),  //這邊改正樣本的.zip檔檔名
+  negative_examples: fs.createReadStream('./cpunegative.zip')   //這邊改負樣本的.zip檔檔名
 };
 
 visual_recognition.createClassifier(params, 
